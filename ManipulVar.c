b7984eb49a85b5a8f3f492d6508ec42c7361c0e9
a20871 <73696024+a20871@users.noreply.github.com>
2020-12-01 17:05:05 +0000

    Update ManipulVar.c
    Teste commit no browser

--- ManipulVar.c
+++ ManipulVar.c
@@ -1,12 +1,12 @@
 /*****************************************************************//**
  * \file   ManipulVar.c
- * \brief  ExercÌcio 1 (Manipular Vari·veis I) da folha de exercÌcios I
+ * \brief  Exerc√≠cio 1 (Manipular Vari√°veis I) da folha de exerc√≠cios I
  * 
- * 1. Sabendo que uma circunferÍncia tem o raio de 2cm, calcule a sua ·rea e perÌmetro. Assuma que PI=3.14.
- * 2. Sabendo que um triangulo tem de base 1.5cm e de altura 2cm, calcula a sua ·rea.
- * 3. Sabendo que um aluno teve as notas 12, 13.4 e 15 nos trÍs testes de uma unidade curricular, qual foi a mÈdia aritmÈtica que obteve no final?
+ * 1. Sabendo que uma circunfer√™ncia tem o raio de 2cm, calcule a sua √°rea e per√≠metro. Assuma que PI=3.14.
+ * 2. Sabendo que um triangulo tem de base 1.5cm e de altura 2cm, calcula a sua √°rea.
+ * 3. Sabendo que um aluno teve as notas 12, 13.4 e 15 nos tr√™s testes de uma unidade curricular, qual foi a m√©dia aritm√©tica que obteve no final?
  * 
- * \author SÈrgio
+ * \author S√©rgio
  * \e-mail a20871@alunos.ipca.pt
  * \date   01 December 2020
  *********************************************************************/
@@ -20,71 +20,69 @@
 
 int main() {
 	/*
-	* Inclus„o de caracteres especiais para escrita correta de portuguÍs.
+	* Inclus√£o de caracteres especiais para escrita correta de portugu√™s.
 	*/
 	setlocale(LC_ALL, "");
 
 
-	float raio = 4; ///< vari·vel raio prÈ determinada
+	float raio = 4; ///< vari√°vel raio pr√© determinada
 
 	/**
-	 * DeterminaÁ„o da ·rea da circunferÍncia.
+	 * Determina√ß√£o da √°rea da circunfer√™ncia.
 	 * 
 	 * \param raio
-	 * \return ·rea da circunferÍncia
+	 * \return √°rea da circunfer√™ncia
 	 */
 	float areaCircunf = (PI * (raio * raio));
 
 	/**
-	 * DeterminaÁ„o do perÌmetro da circunferÍncia.
+	 * Determina√ß√£o do per√≠metro da circunfer√™ncia.
 	 * 
-	 * \param raio ---È o raio
+	 * \param raio ---√© o raio
 	 * \param PI
-	 * \return perÌmetro da circunferÍncia
+	 * \return per√≠metro da circunfer√™ncia
 	 */
 	float perimCircunf = (2 * PI * raio);
 	
 	/// <summary>
-	/// ApresetaÁ„o dos resultados ao utilizador
+	/// Apreseta√ß√£o dos resultados ao utilizador
 	/// </summary>
-	/// <returns>¡rea e perÌmetro da circunferÍncia</returns>
-	printf("a ·rea da circunferencia e': %.2f cm2\n", areaCircunf);
+	/// <returns>√Årea e per√≠metro da circunfer√™ncia</returns>
+	printf("a √°rea da circunferencia e': %.2f cm2\n", areaCircunf);
 
 	printf("O perimetro da circunferencia e': %.2f cm\n", perimCircunf);
 
 
-	float base = 1.5;				/**introduÁ„o do valor da vari·vel base do tri‚ngulo*/
-	float alt = 2;					///introduÁ„o do valor da vari·vel alturda do tri‚ngulo
+	float base = 1.5;				/**introdu√ß√£o do valor da vari√°vel base do tri√¢ngulo*/
+	float alt = 2;					///introdu√ß√£o do valor da vari√°vel alturda do tri√¢ngulo
 
 
 	/**
-	 * C·lculo da ·rea do tri‚ngulo.
+	 * C√°lculo da √°rea do tri√¢ngulo.
 	 * 
 	 * \param base
 	 * \param alt
-	 * \return ·rea do tri‚ngulo especificado
+	 * \return √°rea do tri√¢ngulo especificado
 	 */
 	float areaTriang = ((base * alt) / 2);
 
 	/*
-	ApresentaÁ„o de resultados da ·rea do triangulo ao utilizador.
+	Apresenta√ß√£o de resultados da √°rea do triangulo ao utilizador.
 	*/
 	printf("a area do triangulo e': %.2f cm2", areaTriang);
 
 
 	/*
-	IndicaÁ„o das 3 notas do aluno
+	Indica√ß√£o das 3 notas do aluno
 	*/
 	float nota1 = 12, nota2 = 13.4, nota3 = 15;
 
 	/**
-	 * C·lculo da mÈdia das notas.
+	 * C√°lculo da m√©dia das notas.
 	 * 
 	 * \return 
 	 */
 	float media = ((nota1 + nota2 + nota3) / 3);
-	printf("A mÈdia do aluno È: %.2f", media);
-
-
+	printf("A m√©dia do aluno √©: %.2f", media);
 
 }
